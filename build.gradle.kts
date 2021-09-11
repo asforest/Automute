8252107ef968726b27eef58ce19bee5279f8b6dd
asforest <asforest@qq.com>
2021-09-11 20:21:52 +0800

    chore: mirai依赖版本升级到2.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.7-RC"
+    id("net.mamoe.mirai-console") version "2.7.0"
 }
 
 group = "org.example.qqgroupadmin"
